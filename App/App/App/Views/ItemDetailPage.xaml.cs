9746d3951e85eaf37e4d3c784794a800ae39c41a
sch3fr <sch3fr@gmail.com>
2021-06-04 10:25:41 +0200

    Added basic visual studio android app template

---
+++ App/App/App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using App.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace App.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ App/App/App/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using App.Models;
+using App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
